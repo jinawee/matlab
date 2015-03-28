afab690dcda5af1a35ed6b668cfe9a4a8e61fb0a
jinawee <jinawee.mas1@yahoo.es>
2015-03-28 11:53:05 +0100

    deleted:    Derivacion/cfdn.m 	deleted:    Derivacion/dfx.m 	deleted:    Derivacion/fd.m 	deleted:    Ejercicios/cheby.m 	deleted:    Ejercicios/dd.m 	deleted:    Ejercicios/ecadveccion_perd.m 	deleted:    Ejercicios/ej14.m 	deleted:    Ejercicios/ej2.m 	deleted:    Ejercicios/ifn.m 	deleted:    Ejercicios/untitled.jpg 	deleted:    Examples/fd1d_advection_lax.c 	deleted:    Examples/fd1d_advection_lax.cpp 	deleted:    Examples/fd_neumann.py 	deleted:    Examples/lax.py 	deleted:    Integracion/adicionales/intcompuesta.m 	deleted:    Integracion/adicionales/integralcompuesta.m 	deleted:    Integracion/adicionales/legendrepol.m 	deleted:    Integracion/cfin.m 	deleted:    Integracion/cfinoficial.m 	deleted:    Integracion/gs.m 	deleted:    Integracion/ifab.m 	deleted:    Integracion/simpsonc.m 	deleted:    Integracion/simpsoncvec.m 	deleted:    Interpolacion/captura.m 	deleted:    Interpolacion/cheby.m 	deleted:    Interpolacion/dd.m 	deleted:    Interpolacion/didi.m 	deleted:    Interpolacion/difdiv.m 	deleted:    Interpolacion/ifn.m 	deleted:    Interpolacion/runge.m 	deleted:    ODE/euler.m 	deleted:    ODE/eulerprueba.m 	deleted:    ODE/eulerv.m 	deleted:    ODE/eulervmejorado.m 	deleted:    ODE/orbita.m 	deleted:    ODE/rk4v.m 	deleted:    ODE/rungeKutta.m 	deleted:    PDE/advec.fig 	deleted:    PDE/advec.jpg 	deleted:    PDE/adveclax.jpg 	deleted:    PDE/advection.pdf 	deleted:    PDE/btcs.m 	deleted:    PDE/burger.m 	deleted:    PDE/calor.m 	deleted:    PDE/e_ondas.m 	deleted:    PDE/ecadveccion.m 	deleted:    PDE/ecadveccion_lax.m 	deleted:    PDE/ecadveccion_laxw.m 	deleted:    PDE/ecadveccion_perd.m 	deleted:    PDE/ecburger.m 	deleted:    PDE/eccalor.m 	deleted:    PDE/eccalor2.m 	deleted:    PDE/energia.m 	deleted:    PDE/estacionario.jpg 	deleted:    PDE/ftcs.m 	deleted:    PDE/prueba.m 	deleted:    Prac1/eccalor2.m 	deleted:    Prac1/ecfuentes.m 	deleted:    Prac1/energia.m 	deleted:    Prac1/estacionario.jpg 	deleted:    Prac1/untitled.jpg 	deleted:    aprox.m 	deleted:    burger.m 	deleted:    eccalor.m 	deleted:    mnaf.txt 	Luis/ 	Noriega/ 	PrimeraParte/ 	resumen_primera.txt

--- Interpolacion/cheby.m
+++
@@ -1,8 +0,0 @@
-function x=cheby (n)
-%construye un vector con los nodos de Chebyshev
-%n numero de nodos
-%x vector conteniendo los nodos
-
-k=1:(n-1)
-x=-cos((2*k+1)/(2*n) * pi)
-
--- aprox.m
+++
@@ -1,3 +0,0 @@
-function v=aprox(x,n)
-
-v=round(x*10^n)/10^n;
